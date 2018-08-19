6dcbf1340f94ea39436421767a159028f9926283
Gabriel Freire <gabrielfreiredev@gmail.com>
2018-08-19 15:07:32 +0100

    Better model and prediction method

--- src/networks/super_res/super_res.py
+++ src/networks/super_res/super_res.py
@@ -7,6 +7,7 @@ import numpy as np
 from PIL import Image
 import base64
 import io
+from model import SRNet
 
 
 def raw_to_pil(image_url):
@@ -30,31 +31,34 @@ def predict(model, image, plot=True):
     y, cb, cr = inp.split()
     input = img_to_tensor(y).view(1, -1, y.size[1], y.size[0])
     print('Predicting...')
-    out = model(input.cpu())
-    out = out.cpu();
-    print('Done...')
-    out_img_y = out[0].detach().numpy()
-    out_img_y *= 255.0
-    out_img_y = out_img_y.clip(0, 255)
-    out_img_y = Image.fromarray(np.uint8(out_img_y[0]), mode='L')
-    out_img_cb = cb.resize(out_img_y.size, Image.BICUBIC)
-    out_img_cr = cr.resize(out_img_y.size, Image.BICUBIC)
-    out_img = Image.merge('YCbCr', [out_img_y, out_img_cb, out_img_cr]).convert('RGB')
-    if(plot):
-        plt.figure(figsize=(20,20))
-        plt.title('Input')
-        plt.imshow(inp.convert('RGB'))
+    with torch.no_grad():
+        out = model(input.cpu())
+        out = out.cpu();
+        print('Done...')
+        out_img_y = out[0].detach().numpy()
+        out_img_y *= 255.0
+        out_img_y = out_img_y.clip(0, 255)
+        out_img_y = Image.fromarray(np.uint8(out_img_y[0]), mode='L')
+        out_img_cb = cb.resize(out_img_y.size, Image.BICUBIC)
+        out_img_cr = cr.resize(out_img_y.size, Image.BICUBIC)
+        out_img = Image.merge('YCbCr', [out_img_y, out_img_cb, out_img_cr]).convert('RGB')
+        if(plot):
+            plt.figure(figsize=(20,20))
+            plt.title('Input')
+            plt.imshow(inp.convert('RGB'))
 
-        plt.figure(figsize=(20,20))
-        plt.title('Output')
-        plt.imshow(out_img)
-        plt.show()
+            plt.figure(figsize=(20,20))
+            plt.title('Output')
+            plt.imshow(out_img)
+            plt.show()
 
     return out_img
 
 def execute(base64):
     # load model and predict
-    model = torch.load(os.getcwd() + '/assets/models/superres/model_epoch_50.pth', map_location='cpu')
+    upscale_factor = 3
+    model = SRNet(upscale_factor=upscale_factor)
+    model.load_state_dict(torch.load(os.getcwd() + '/assets/models/superres/model_epoch_150.pth', map_location='cpu'))
     model.eval()
     image = base64_to_pil_img(base64)
     out_img = predict(model, image, plot=False)
